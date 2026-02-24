41c60c984781101a5bc3d0688cd156687b493e18
VadimMMR <vadimbarsov51@gmail.com>
2026-02-24 22:36:26 +0200

    fix v5

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import sys
 import os
-from device_lib.main import get_device_info
+from hardware_collector_lib.main import get_device_info
 import json
 
 def get_info_device():
     all_info = get_device_info()
     print(all_info)
-
-    for key, value in all_info["cpu"].items():
-        print(f"{key:20}: {value}")
-    for gpu in all_info["gpu"]:
-        for key, value in gpu.items():
-            print(f"{key:20}: {value}")
-    for key, value in all_info["ram"].items():
-        print(f"{key:20}: {value}")
 
 if __name__ == "__main__":
     get_info_device()
