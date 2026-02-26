615d6f6095b99b876ca99930af6af60635427885
VadimMMR <vadimbarsov51@gmail.com>
2026-02-26 11:20:08 +0200

    new lib fix4

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from os_system_lib.main import get_complete_os_info
 import json
 
 def get_info_device():
-    system_info = get_complete_os_info
+    system_info = get_complete_os_info()
     all_info = get_device_info()
     print(system_info)
     print(all_info)
